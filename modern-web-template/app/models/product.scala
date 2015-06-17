0df80519d8716bfe23b15750d2f9972ce8edc6e8
EricLin-wk <superic@gmail.com>
2015-06-17 16:39:17 -0700

    entity objects

---
+++ modern-web-template/app/models/product.scala
@@ -0,0 +1,20 @@
+package models
+
+import java.util.Date
+
+case class Product(productId: Int,
+                   name: String,
+                   categoryId: Int,
+                   price: BigDecimal,
+                   description: String,
+                   qty: Int,
+                   attribute: Array[String],
+                   createDate: Date)
+
+object JsonFormats {
+
+  import play.api.libs.json.Json
+
+  // Generates Writes and Reads for Feed and User thanks to Json Macros
+  implicit val userFormat = Json.format[Product]
+}
