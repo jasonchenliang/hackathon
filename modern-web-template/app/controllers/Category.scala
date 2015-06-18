e871cd641b3b5a5e03412fbf0d90e3504a4a64b6
unknown <gavin.liu@WRK-IT-42.zepower.com>
2015-06-17 17:37:32 -0700

    Category Controller

---
+++ modern-web-template/app/controllers/Category.scala
@@ -0,0 +1,11 @@
+package controllers
+
+import play.api.mvc.Controller
+import play.modules.reactivemongo.MongoController
+
+/**
+ * Created by gavin.liu on 6/17/2015.
+ */
+class Category extends Controller with MongoController {
+
+}
